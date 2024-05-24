649b3c58ea827470aa15b0adf48cb9f62ff338ea
Paweł Chudecki <pawelc29@interia.pl>
2024-05-24 16:25:09 +0200

    working CRUD

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { FaTrashAlt } from "react-icons/fa";
+import Goal from "./components/Goal";
 import { IoIosAddCircle } from "react-icons/io";
 
 function App() {
@@ -13,7 +13,6 @@ function App() {
       // .get("http://localhost:5000/api/goals")
       .get("https://goal-setter-app-mocha.vercel.app/api/goals")
       .then((response) => {
-        console.log(response.data);
         setData(response.data);
       })
       .catch((error) => {
@@ -33,54 +32,67 @@ function App() {
       })
       .then(() => {
         getHandler();
-        // setShowInput(false);
+        setShowInput(false);
+        setInputText("");
       });
   };
 
-  const deleteHandler = (id) => {
-    axios
-      // .delete(`http://localhost:5000/api/goals/${id}`)
-      .delete(`https://goal-setter-app-mocha.vercel.app/api/goals/${id}`)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .then(() => {
-        getHandler();
-      });
-  };
+  useEffect(() => {
+    getHandler();
+  }, []);
 
-  // getHandler();
   return (
     <div className="w-screen flex flex-col items-center justify-between min-h-screen ">
       <main className=" border-gray-600 w-screen max-w-md">
+        {/* HEADER */}
         <h1 className="text-4xl p-1 text-center text-cyan-500 font-bold border-b-2 border-b-cyan-800">
           Goal Setter
         </h1>
-        <nav className="flex justify-center gap-4 m-4">
+        <p className="text-cyan-800 text-center">
+          MERN (MongoDB, Express.js, React, and Node.js) stack app
+        </p>
+        {/* NAVIGATION */}
+        {/* <nav className="flex justify-center gap-4 m-4">
           <button
             className="bg-cyan-900 rounded-md px-2 py-1 uppercase font-semibold hover:bg-cyan-500 hover:text-cyan-950"
             onClick={getHandler}
           >
             download list{" "}
           </button>
-          <button
-            className="bg-cyan-900 rounded-md px-2 py-1 uppercase font-semibold hover:bg-cyan-500 hover:text-cyan-950"
-            onClick={() => setShowInput(!showInput)}
-          >
-            open input
-          </button>
-        </nav>
-        <ul className="flex flex-col gap-4 px-4">
-          {showInput && (
+        </nav> */}
+        <ul className="flex flex-col gap-4 px-4 mt-4">
+          {data.length > 0 ? (
+            // GOALS
+            data.map((item, index) => (
+              <Goal
+                item={item}
+                key={item["_id"]}
+                index={index}
+                data={data}
+                setData={setData}
+              />
+            ))
+          ) : (
+            <p className="text-center text-xl m-2">No goals</p>
+          )}
+          {!showInput ? (
+            <button
+              className="bg-cyan-900 rounded-xl px-2 py-2 uppercase font-semibold hover:bg-cyan-500 hover:text-cyan-950"
+              onClick={() => setShowInput(!showInput)}
+            >
+              add new
+            </button>
+          ) : (
             <form className="border-2 border-cyan-900 rounded-xl p-1 flex items-center bg-cyan-200 bg-opacity-70 hover:drop-shadow-[0_0_6px_rgba(21,94,117,0.6)] ">
               <input
-                className="bg-transparent text-black font-semibold placeholder:text-zinc-800 w-full mr-1 outline-none"
+                className="new-goal bg-transparent text-black font-semibold placeholder:text-zinc-700 w-full mr-1 outline-none"
                 type="text"
                 name="text-input"
                 id="text-input"
-                placeholder="Add goal"
+                placeholder="New goal"
                 onChange={(e) => setInputText(e.target.value)}
                 onFocus={(e) => (e.target.value = "")}
+                autoFocus
               />
               <button
                 type="submit"
@@ -95,33 +107,9 @@ function App() {
               </button>
             </form>
           )}
-          {data.length > 0 &&
-            data.map((item, index) => (
-              <li
-                key={item.id}
-                className="relative border-2 border-cyan-900 rounded-xl p-1 flex items-center justify-start bg-cyan-950 hover:drop-shadow-[0_0_6px_rgba(21,94,117,0.6)] hover:brightness-125"
-              >
-                <div className="text-xl border-cyan-900 border-2 rounded-full size-10 grid items-center justify-center pb-[2px] mx-2 aspect-square">
-                  {index + 1}
-                </div>
-                <div className="w-full mr-10">
-                  <div className="text-xl">{item.text}</div>
-                  <div className="opacity-50 text-sm border-t mt-1">
-                    Date: {item.createdAt.slice(0, 10)} Time:{" "}
-                    {item.createdAt.slice(11, 19)}
-                  </div>
-                </div>
-                <button
-                  title="delete"
-                  className="mr-2 size-6 grid place-items-center absolute right-0 hover:text-red-700 transition-colors"
-                  onClick={() => deleteHandler(item["_id"])}
-                >
-                  <FaTrashAlt />
-                </button>
-              </li>
-            ))}
         </ul>
       </main>
+      {/* FOOTER */}
       <footer className="text-center mt-4 border-t border-cyan-900 w-full mx-auto text-gray-500 p-1 font-medium">
         Paweł Chudecki &copy; 2024
       </footer>
---
+++ frontend/src/components/Goal.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { FaTrashAlt } from "react-icons/fa";
+import { useState } from "react";
+import axios from "axios";
+
+const Goal = ({ item, index, data, setData }) => {
+  const [goalValue, setGoalValue] = useState(item.text);
+
+  const getHandler = () => {
+    axios
+      // .get("http://localhost:5000/api/goals")
+      .get("https://goal-setter-app-mocha.vercel.app/api/goals")
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const editHandler = (id, text) => {
+    axios
+      // .post("http://localhost:5000/api/goals", {
+      .put(`https://goal-setter-app-mocha.vercel.app/api/goals/${id}`, {
+        text: text,
+      })
+
+      .then((response) => {
+        console.log(response.data);
+      })
+      .then(() => {
+        getHandler();
+        // setShowInput(false);
+      });
+  };
+
+  const deleteHandler = (id) => {
+    axios
+      // .delete(`http://localhost:5000/api/goals/${id}`)
+      .delete(`https://goal-setter-app-mocha.vercel.app/api/goals/${id}`)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .then(() => {
+        getHandler();
+      });
+  };
+  return (
+    <li
+      key={item["_id"]}
+      className="relative border-2 border-cyan-900 rounded-xl p-1 flex items-center justify-start bg-cyan-950 hover:drop-shadow-[0_0_6px_rgba(21,94,117,0.6)] hover:brightness-125 "
+    >
+      <div className="text-xl border-cyan-900 border-2 rounded-full flex items-center justify-center size-8 aspect-square pb-[2px] mx-2 ">
+        {index + 1}
+      </div>
+      <div className="w-[80%] ">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            e.target.children[0].blur();
+            editHandler(item["_id"], e.target.text.value);
+          }}
+        >
+          <input
+            onChange={(e) => setGoalValue(e.target.value)}
+            onBlur={(e) => editHandler(item["_id"], e.target.value)}
+            className="bg-transparent text-xl focus:outline-none focus:bg-cyan-900 placeholder:text-white w-full cursor-pointer"
+            value={goalValue}
+            type="text"
+            name="text"
+            id={item.id}
+          />
+        </form>
+
+        <div className="opacity-50 text-sm border-t mt-1">
+          Created: {item.createdAt.slice(0, 10)} /{" "}
+          {item.createdAt.slice(11, 19)}
+        </div>
+      </div>
+      <button
+        title="delete"
+        className="mr-2 size-6 grid place-items-center absolute right-0 hover:text-red-700 transition-colors"
+        onClick={() => deleteHandler(item["_id"])}
+      >
+        <FaTrashAlt />
+      </button>
+    </li>
+  );
+};
+
+export default Goal;
